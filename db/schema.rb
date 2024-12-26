202771f65b020f5e3b930ea04603e36212541403
kakinotane77 <kakitsubo.yasuaki.77@gmail.com>
2024-12-26 16:53:13 +0900

    ActiveHash実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_25_062147) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_26_070556) do
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_fee_burden_id", null: false
+    t.integer "shipping_form_location_id", null: false
+    t.integer "delivery_time_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
-    t.string "nickname", null: false
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
@@ -34,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_25_062147) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
