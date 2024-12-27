60822ef3c52b1b6c962877c994f12ec95bb3e730
kakinotane77 <kakitsubo.yasuaki.77@gmail.com>
2024-12-27 18:00:28 +0900

    単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,26 +1,35 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_time
   belongs_to :shipping_fee_burden
   belongs_to :shipping_form_location
-
-  validates :item_name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true, numericality: {
-    only_integer: true,
-    greater_than_or_equal_to: 300,
-    less_than_or_equal_to: 9_999_999
-  }
-
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_fee_burden_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_form_location_id, numericality: { other_than: 1, message: "can't be blank" }
-
+  belongs_to :user
   has_one :purchase_record
   has_one_attached :image
-  belongs_to :user
+
+  validates :item_name, presence: { message: "can't be blank" }
+  validates :description, presence: { message: "can't be blank" }
+
+  validates :price, presence: { message: "can't be blank" }
+  validates :price, format: { with: /\A\d+\z/, message: 'must be a valid half-width number' },
+                    if: -> { price.present? }
+  validates :price,
+            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                            message: 'must be a valid half-width number between 300 and 9,999,999' },
+            if: -> { price.present? }
+
+  validates :category_id, :condition_id, :delivery_time_id,
+            :shipping_fee_burden_id, :shipping_form_location_id,
+            numericality: { other_than: 1, message: "can't be blank" }
+
+  validate :image_presence
+
+  private
+
+  def image_presence
+    errors.add(:image, "can't be blank") unless image.attached?
+  end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { Faker::Name.initials(number: 2) }
+    description { Faker::Lorem.sentence }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    condition_id { Faker::Number.between(from: 2, to: 6) }
+    shipping_fee_burden_id { Faker::Number.between(from: 2, to: 3) }
+    shipping_form_location_id { Faker::Number.between(from: 2, to: 48) }
+    delivery_time_id { Faker::Number.between(from: 2, to: 4) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
