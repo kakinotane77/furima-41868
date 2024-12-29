8902a4bafcf70ee74b27518f8629684d45b82eb0
kakinotane77 <kakitsubo.yasuaki.77@gmail.com>
2024-12-29 15:24:49 +0900

    ログイン状態の有無判定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -41,7 +42,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return if user_signed_in? && @item.user_id == current_user.id
+    return if @item.user_id == current_user.id
 
     redirect_to root_path
   end
